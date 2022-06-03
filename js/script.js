8e327a89efb0505a6821a3bfb10a15a03196515a
brenoborba <brenoborba99@gmail.com>
2022-06-03 22:08:03 +0200

    Style alteration and core Javascript code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
-const slider = document.querySelector(".slider")
+const name = document.getElementById('name')
+const mensalidade = document.getElementById('mensalidade')
+const taxaJuros = document.getElementById('taxa')
+const form = document.getElementById('form')
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    let inputName = name.value
+    let inputMensalidade = mensalidade.value
+    let inputTaxaJuros = taxaJuros.value
+    let inputSlider = slider.value
+
+    console.log(inputTaxaJuros/100)
+
+
+    let main = {
+        "expr": `${inputMensalidade} * (((1 * ${inputTaxaJuros/100}))) ^ ${inputSlider * 12}) / ${inputTaxaJuros/100}`
+    }
+
+    let mainFinal = JSON.stringify(main)
+
+    console.log(mainFinal)
+
+})
+
+
+
+
+
+//SLIDER
+const slider = document.getElementById("slider")
 const output = document.querySelector(".slider-output")
 output.innerHTML = `${slider.value} ano`
 
