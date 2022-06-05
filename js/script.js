351c63148daae9cabc07a9a112ae03d0719bc466
brenoborba <brenoborba99@gmail.com>
2022-06-05 16:06:36 +0200

    API post + form refactor

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const name = document.getElementById('name')
 const mensalidade = document.getElementById('mensalidade')
 const taxaJuros = document.getElementById('taxa')
 const form = document.getElementById('form')
+const btn = document.querySelector('.btn')
+
+const url = "http://api.mathjs.org/v4/"
 
 //FORM SUBMISSION
 form.addEventListener('submit', (e) =>{
@@ -12,17 +15,36 @@ form.addEventListener('submit', (e) =>{
     let inputTaxaJuros = taxaJuros.value
     let inputSlider = slider.value
 
-    console.log(inputTaxaJuros/100)
-
-
     let main = {
-        "expr": `${inputMensalidade} * (((1 * ${inputTaxaJuros/100}))) ^ ${inputSlider * 12}) / ${inputTaxaJuros/100}`
+        expr: `
+            ${inputMensalidade} * (((1 + (${inputTaxaJuros} * 0.01)) ^ ${inputSlider * 12} - 1) / (${inputTaxaJuros} * 0.01))
+        `
     }
+    
 
-    let mainFinal = JSON.stringify(main)
-    console.log(mainFinal)
+    api(main)
+}) 
 
+//API POST
+
+const test = {"expr": `20 * (((1 + 0.00517) ^ 24 - 1) / 0.00517)`}
+
+
+const configs = (expr) => ({
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify(test)
 })
+
+async function api(expr){
+    return fetch(url, configs(expr))
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
 
 //SLIDER
 const slider = document.getElementById("slider")
@@ -36,27 +58,3 @@ slider.oninput = function () {
         output.innerHTML = `${this.value} ano`
     }
 }
-
-
-//API POST
-const url = "http://api.mathjs.org/v4/"
-
-const test = {"expr": "20 * (((1 + 0.00517) ^ 24 - 1) / 0.00517)"}
-
-const configs = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-        "Content-type": "application/json"
-    },
-    body: JSON.stringify(test)
-}
-
-async function api(){
-    return fetch(url, configs)
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
-
-api()
-
